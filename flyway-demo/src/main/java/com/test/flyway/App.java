817044f8f25b43fb0afa9005c6da943ec5ca8d19
ping.liu 刘平 <ping.liu@qiyuesuo.com>
2018-12-03 11:48:15 +0800

    flyway App

--- flyway-demo/src/main/java/com/test/flyway/App.java
+++ flyway-demo/src/main/java/com/test/flyway/App.java
@@ -4,10 +4,9 @@ import com.mysql.jdbc.Driver;
 import org.flywaydb.core.internal.jdbc.DriverDataSource;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Hello world!
- */
 public class App {
 	public static void main(String[] args) {
 		String url = "jdbc:mysql://127.0.0.1:3306/flyway?useUnicode=true&useSSL=false&characterEncoding=utf-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&aserverTimezone=UTC";
